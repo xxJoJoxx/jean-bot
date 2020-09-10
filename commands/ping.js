d0fc457c46cbf743838c464b1e8187ec4b344a48
xxJoJoxx <jklz521@gmail.com>
2020-09-09 22:29:48 -0500

    added and additional field for help

--- commands/ping.js
+++ commands/ping.js
@@ -8,7 +8,7 @@ module.exports = {
 		try {
 			const msg = await message.channel.send('Ping is being appreciated... :slot_machine:');
 			const ping = Math.round((msg.createdTimestamp - message.createdTimestamp));
-			msg.edit(`Ping: \`${ping} ms\` :race_car::dash:`);
+			msg.edit(`Pong: \`${ping} ms\` :race_car::dash:`);
 		}
 		catch (error) {
 			console.error(error);
