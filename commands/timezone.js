495d1583ba03551bd91d9dcfc702c0371d318d07
xxJoJoxx <jklz521@gmail.com>
2020-09-09 22:31:19 -0500

    added example and logic to parse and convert time

--- commands/timezone.js
+++ commands/timezone.js
@@ -1,10 +1,18 @@
+const moment = require('moment');
+
 module.exports = {
 	name: 'localtime',
 	aliases: ['lt'],
+	usage: '[command name/alias]',
+	example: 'August 18 23:59:59 GMT+8',
 	description:
-		'Converts a given time and timezone to the requestor\'s local time',
+		'Converts a given date, time and timezone to the requestor\'s local date and time',
 	guildOnly: true,
-	execute(message, time) {
-		message.channel.send(`Your local time will be ${time}`);
+	execute(message, args) {
+		args = args.map((arg) => arg.replace('th', ''));
+		(!moment(args[2], 'YYYY', true).isValid()) && args.splice(2, 0, new Date().getFullYear());
+		const userDateTime = args.join(' ');
+		const utcTime = new Date(userDateTime);
+		message.channel.send(`Your local time would be \`${utcTime}\``);
 	},
 };
